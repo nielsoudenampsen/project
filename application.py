4df309efd9b5f68c612a81116fdcfab962aa252b
Niels <nielsoudenampsen@hotmail.com>
2021-11-27 17:59:38 +0100

    added autofocus to search and providing id to pdp

--- application.py
+++ application.py
@@ -27,16 +27,15 @@ Session(app)
 db = SQL("sqlite:///recipe.db")
 
 # Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
+# if not os.environ.get("API_KEY"):
+#     raise RuntimeError("API_KEY not set")
 
 @app.route('/recipe/<id>',methods=['GET', 'POST'])
 @login_required
 def recipe_detail(id):
-    id = request.args.get("id")
-    details = recipe_detail(id)
-    print(details)
-    return redirect("/")
+    pageid = id
+    
+    return render_template("recipe_detail.html", id=pageid)
 
 
 @app.route('/',methods=['GET', 'POST'])
