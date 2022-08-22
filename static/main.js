7d6a06b4213268d62ec2260594b83fcc2a7e8aea
Niels <nielsoudenampsen@hotmail.com>
2022-08-22 12:29:10 +0200

    added automatic login function and bug fixes

--- static/main.js
+++ static/main.js
@@ -1,15 +1,14 @@
+// sends post request with ajax, so the page is not reloaded with every button click
 $(document).ready(function(){
     $(".favorite_button").click(function(){
         let favorites = document.getElementsByClassName("favorite_button");
         let itemid = $(this).closest(".card").attr("id")
-        
         
         for (let i = 0; i < favorites.length; i++) {
             let clickedid = favorites[i].getAttribute('data-arg1')
             if(itemid == clickedid){
                 console.log(itemid)
                 $.ajax({
-                        
                     type : 'POST',
                     url : "/favorite/" + favorites[i].getAttribute('data-arg1'),
                     contentType: 'application/json;charset=UTF-8',
@@ -27,17 +26,27 @@ $(document).ready(function(){
                 }
             }
           }
-        
-
-        
-     
     });
   });
 
+  //updates the favorites list when the button is pressed
 $(document).ready(function(){
     $(".update-button").click(function(){
         location.reload();
         return false;
     });
     
+});
+
+
+// DEBUG only, automaticly login
+$(document).ready(function(){
+
+    username_login = document.getElementById("username_login").setAttribute("value","n")
+    password_login = document.getElementById("password_login").setAttribute("value","1")
+    
+    let path = window.location.pathname
+    if(path == '/login' || path == '/logout' ) {
+        document.login.submit();
+    } 
 });
